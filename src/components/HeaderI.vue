9006fbb9c9881e8917569e5e9f616a7b4e765de8
Accobat <podrugin@hotmail.com>
2022-11-11 15:54:05 +0300

    State and Getters

---
+++ src/components/HeaderI.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    HeaderI
+    {{ usersList }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "HeaderI",
+  computed: {
+    ...mapGetters("users", ["usersList"]),
+  },
+};
+</script>
